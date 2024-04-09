34af09e615f629909a694a3c2b3e70c286c53f5a
JsoNJ552 <jimenezjason35@gmail.com>
2024-04-09 15:33:13 -0600

    Vista Productos

---
+++ CrediV1_Prueba/Controllers/InventarioController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CrediV1_Prueba.Controllers
+{
+    public class InventarioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult AgregarProducto()
+        {
+            return View();
+        }
+
+		public IActionResult ListadoProducto()
+		{
+			return View();
+		}
+
+        public IActionResult EditarProducto() { 
+        
+        
+            return View();
+        }   
+
+
+
+	}
+}
