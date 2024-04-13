6ed5a61d9dadcddb3fee3af291004f84b38bdd92
bry245 <bryvm2524@gmail.com>
2024-04-12 20:29:54 -0600

    12/4

--- CrediV1_Prueba/obj/Debug/net8.0/CrediV1_Prueba.AssemblyInfo.cs
+++ CrediV1_Prueba/obj/Debug/net8.0/CrediV1_Prueba.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CrediV1_Prueba")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b899f780a83cefe360ca139cbc05f492c26eef6f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b151283ab00d6d47d5d7ab6a7192006bf28c9b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("CrediV1_Prueba")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CrediV1_Prueba")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
